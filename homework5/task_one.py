4713b88c4d7fb41ff2d1f13d4945f2186fbb9614
Vihnovskaya Vika <vihnovskaya1932@gmail.com>
2022-12-10 18:42:20 +0300

    Добавила вывод первой задачи в текстовый файл

--- homework5/task_one.py
+++ homework5/task_one.py
@@ -8,9 +8,11 @@
 #     if 'абв' not in i:
 #         my_list.append(i)
 # print(*my_list)
-
-str_Text = 'Я хочу классно иабв продуктивноабв кодить на Джавеабв иабв Питоне'
-print(' '.join([i for i in str_Text.split(' ') if 'абв' not in i]))
+with open("task_1.txt", "r+") as data:
+    str_Text = data.readline()
+    print(str_Text)
+    data.write(' '.join([i for i in str_Text.split(' ') if 'абв' not in i]))
+    print(' '.join([i for i in str_Text.split(' ') if 'абв' not in i]))
 
 
 
